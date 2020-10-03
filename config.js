d24d7147daf1ccd875b567918b080a827e997c2a
Reva724 <71178082+Reva724@users.noreply.github.com>
2020-10-03 19:38:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
   module.exports = {
-    TOKEN: "NzYxNjEzNjk0NTYzNzc4NjAw.X3dKBg.7Z8XRUJGmRhzAmMyITeSGLblAu4",////تۆکین
+    TOKEN: "NzYxNjEzNjk0NTYzNzc4NjAw.X3dKBg.HrEPHKmC_458xE7vYTdPDYHpWMQ",////تۆکین
     prefix: ">",
     devs: ["725424273510105178"]
   };
